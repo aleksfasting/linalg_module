41bab279d196b86b73cb105f42ec41496a487791
Aleks Fasting <aleks.fasting@gmail.com>
2024-07-08 01:02:41 +0200

    added string method to matrix

--- Matrix.py
+++ Matrix.py
@@ -1,9 +1,6 @@
-
-
 class Matrix:
-    array = []
-    
     def __init__(self, dim: (int,int), vals = None):
+        self.dim = dim
         if (vals == None):
             for i in range(dim):
                 self.array.append(0)
@@ -11,3 +8,9 @@ class Matrix:
             for i in range(dim[0]):
                 for j in range(dim[1]):
                     self.array.append(vals[i][j])
+
+    def __str__(self):
+        for i in range(self.dim[0]):
+            for j in range(self.dim[1]):
+                print(self.array[i][j], end = " ")
+            print()
