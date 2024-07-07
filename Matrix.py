1c6bd6809bddf800206d085422cc66575ad15d28
Aleks Fasting <aleks.fasting@gmail.com>
2024-07-08 01:24:49 +0200

    Added matrix multiplication

--- Matrix.py
+++ Matrix.py
@@ -37,11 +37,21 @@ class Matrix:
             for j in range(self.dim[1]):
                 res.array[i][j] = self.array[i][j] - other.array[i][j]
         return res
+    
+    def __mul__(self: 'Matrix', other: 'Matrix') -> 'Matrix':
+        if (self.dim[1] != other.dim[0]):
+            raise Exception("Invalid matrix dimensions")
+        res = Matrix((self.dim[0], other.dim[1]))
+        for i in range(self.dim[0]):
+            for j in range(other.dim[1]):
+                for k in range(self.dim[1]):
+                    res.array[i][j] += self.array[i][k] * other.array[k][j]
+        return res
 
 def main() -> None:
     m = Matrix((3,3), [[1,2,3],[4,5,6],[7,8,9]])
-    n = Matrix((3,3), [[9,8,7],[6,5,4],[3,2,1]])
-    o = m-n
+    n = Matrix((3,2), [[9,7],[6,4],[3,1]])
+    o = m*n
     print(o)
 
 if __name__ == "__main__":
